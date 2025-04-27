29e61b8d59353225d4f6a2122c9459f27be79a6f
Squizzy <crazy_squirrel@yahoo.com>
2025-04-27 11:07:11 +0100

    the deck of card object

---
+++ deck.py
@@ -0,0 +1,36 @@
+from random import shuffle
+from card import Card, Suit, Rank
+
+class Deck:
+    _cards: list[Card]
+
+    def __init__(self, playerNum:int):
+        self._cards = []
+        for suit in Suit:
+            for rank in Rank:
+                # TODO: add pictures
+                self._cards.append(Card(rank, suit, playerNum))
+
+    @property
+    def cards(self) -> list[Card]:
+        return self._cards
+    
+    @property
+    def size(self) -> int:
+        return len(self._cards)
+
+    def shuffle(self) -> None:
+        shuffle(self._cards)
+
+    def draw_n_cards(self, num_cards:int) -> list[Card]:
+        picked_cards:list[Card] = []
+        for _ in range(num_cards):
+            picked_cards.append(self._cards.pop())
+        return picked_cards
+    
+    def draw_remaining_cards(self) -> list[Card]:
+        picked_cards:list[Card] = self._cards
+        self._cards = []
+        return picked_cards
+
+        
