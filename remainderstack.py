fc27b9b5ab382a9431a137fcc7d8220a8c7dc49a
Squizzy <crazy_squirrel@yahoo.com>
2025-04-29 00:45:31 +0100

    added the reload_from_binstack method

--- remainderstack.py
+++ remainderstack.py
@@ -4,6 +4,7 @@ from deck import Deck
 
 class RemainderStack(CardStack):
     _cards: list[Card]
+    _stackname: str = "Remainder"
 
     def __init__(self, deck: Deck):
         self._cards = []
@@ -16,3 +17,14 @@ class RemainderStack(CardStack):
     # @property
     # def top_card(self):
     #     return self._cards[len(self._cards) - 1]
+    
+    def reload_from_binstack(self, binstack_cards: list[Card]) -> bool:
+        if self._cards:
+            return False
+        
+        new_stack: list[Card] = binstack_cards
+        new_stack.reverse()
+        for card in new_stack:
+            card.turn_face_down()
+            self._cards.append(card)
+        return True
