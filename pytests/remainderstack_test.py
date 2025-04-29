d134c71459c1363faa27bc5edbc7dc4ec910a8d7
Squizzy <crazy_squirrel@yahoo.com>
2025-04-29 17:41:32 +0100

    appended additional path to perform folder-level pytest, moved deck back from sample, changed field calls to porperty calls

--- pytests/remainderstack_test.py
+++ pytests/remainderstack_test.py
@@ -1,21 +1,28 @@
 import sys
 sys.path.append('..')
+sys.path.append('.')
 
 from card import Card #, Rank, Suit
-# from deck import Deck
-from samples import random_stacked_cards, deck
+from deck import Deck
+from samples import random_stacked_cards
 from icecream import ic #type:ignore
 
 from remainderstack import RemainderStack
 player_num: int = 1
 
-deck.draw_n_cards(13+4) # remove the crapette and Tableau cards that won't be distributed before this test here
+# One deck
+deck: Deck = Deck(1)
+
+# ic(deck.size)
+
+deck.draw_n_cards(13) # remove the crapette cards that won't be distributed before this test here
+deck.draw_n_cards(4) # remove the Tableau cards that won't be distributed before this test here
 
 remainder = RemainderStack(deck, player_num)
 
 def test_constructor():
     print ()
-    ic(remainder._stackname)
+    ic(remainder.what_stack_am_i)
     ic(remainder.top_card)
 
     assert remainder.what_stack_am_i == "Remainder"
