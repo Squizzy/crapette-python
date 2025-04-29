37265db32792037d9b10af066dc4109d1ab906ae
Squizzy <crazy_squirrel@yahoo.com>
2025-04-29 16:54:49 +0100

    added player ID reference and improved error checking

--- tableaustacks.py
+++ tableaustacks.py
@@ -3,9 +3,11 @@ from tableaustack import TableauStack
 
 class TableauStacks:
     _TableauStacks: list[TableauStack]
+    _player_num: int
 
-    def __init__(self, deck:Deck):
+    def __init__(self, deck:Deck, player_num: int):
+        self._player_num =  player_num
         self._TableauStacks = []
         for _ in range(4):
-            TS = TableauStack(deck)
+            TS = TableauStack(deck, player_num)
             self._TableauStacks.append(TS)
