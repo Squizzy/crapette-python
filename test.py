f406f4c97444daab43910c8e3dbff3d1b3293363
Squizzy <crazy_squirrel@yahoo.com>
2025-04-29 16:56:10 +0100

    fixed calls now requiring player ID

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from deck import Deck
 from crapettestack import CrapetteStack
 from tableaustacks import TableauStacks
 
+player_num = 1
+
 card1: Card = Card(rank=Rank.FIVE, suit=Suit.CLUBS, player_num=1)
 card2: Card = Card(rank=Rank.SIX, suit=Suit.CLUBS, player_num=1)
 card3: Card = Card(rank=Rank.FIVE, suit=Suit.HEARTS, player_num=1)
@@ -13,7 +15,7 @@ p1deck.shuffle()
 # p1deck_cards = p1deck.list_cards()
 # for card in p1deck_cards:
 #     print(f"{card.value()} of {card.suit()}")
-p1crapette:CrapetteStack = CrapetteStack(p1deck)
+p1crapette:CrapetteStack = CrapetteStack(p1deck, player_num)
 p1crapette_cards = p1crapette.cards
 for card in p1crapette_cards:
     # print(f"{card._rank.display_name} of {card.suit()} - {card.face_up()}")
@@ -46,7 +48,7 @@ for card in p1crapette_cards:
     print(card)
 
 print("### - Add card2")
-p1crapette.add_card(card2)
+p1crapette.add_card(card2, player_num)
 for card in p1crapette_cards:
     print(card)
 
@@ -56,17 +58,17 @@ for card in p1crapette_cards:
     print(card)
 
 print("### - Add card3")
-p1crapette.add_card(card3)
+p1crapette.add_card(card3, player_num)
 for card in p1crapette_cards:
     print(card)
 
 print("### - Add card4")
-p1crapette.add_card(card4)
+p1crapette.add_card(card4, player_num)
 for card in p1crapette_cards:
     print(card)
 
 print("### - Create Tableau")
-tableau: TableauStacks = TableauStacks(p1deck)
+tableau: TableauStacks = TableauStacks(p1deck, player_num)
 print(tableau._TableauStacks[0]._cards[0])
 print(tableau._TableauStacks[1]._cards[0])
 print(tableau._TableauStacks[2]._cards[0])
