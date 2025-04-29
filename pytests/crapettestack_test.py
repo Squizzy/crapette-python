945cd3ff8edef127f6d720585e0ae4aee7c28493
Squizzy <crazy_squirrel@yahoo.com>
2025-04-29 17:39:00 +0100

    appended additional path to perform folder-level pytest, moved deck back from sample, added new player ID to some methods

--- pytests/crapettestack_test.py
+++ pytests/crapettestack_test.py
@@ -1,20 +1,25 @@
 import sys
 sys.path.append('..')
+sys.path.append('.')
 
 # from card import Card #, Rank, Suit
-# from deck import Deck
-from samples import deck, card_8H, card_9H, card_10H, card_7D, card_7S, card_7H
+from deck import Deck
+from samples import card_8H, card_9H, card_10H, card_7D, card_7S, card_7H
 from icecream import ic #type:ignore
 
 
 from crapettestack import CrapetteStack
 player_num:int = 1
 
+# One deck - reset the deck size as 2 cards were taken for the tableaux above
+deck: Deck = Deck(1)
+
 crapette: CrapetteStack = CrapetteStack(deck, player_num)
 
 def test_constructor():
     print()
     ic(crapette.what_stack_am_i)
+    ic(crapette.top_card)
     
     assert crapette.what_stack_am_i == "Crapette"
     assert crapette.size == 13
@@ -24,20 +29,20 @@ def test_can_be_added():
     force_add_card_to_crapette()
     assert crapette.top_card == card_8H
     assert crapette.size == 14
-    assert crapette.can_be_added(card_9H)
-    assert crapette.can_be_added(card_7H)
-    assert not crapette.can_be_added(card_7D)
-    assert not crapette.can_be_added(card_10H)
-    assert not crapette.can_be_added(card_7S)
+    assert crapette.can_be_added(card_9H, player_num)
+    assert crapette.can_be_added(card_7H, player_num)
+    assert not crapette.can_be_added(card_7D, player_num)
+    assert not crapette.can_be_added(card_10H, player_num)
+    assert not crapette.can_be_added(card_7S, player_num)
     
 
 def test_add_card():
     assert crapette.top_card == card_8H
     assert crapette.size == 14
-    crapette.add_card(card_9H)
+    crapette.add_card(card_9H, player_num)
     assert crapette.top_card == card_9H
     assert crapette.size == 15
-    crapette.add_card(card_7D)
+    crapette.add_card(card_7D, player_num)
 
 
 def force_add_card_to_crapette():
