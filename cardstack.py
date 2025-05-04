41220830bcf1f402fe44a0694dff9a597a761339
Squizzy <crazy_squirrel@yahoo.com>
2025-05-04 19:41:07 +0100

    added StacksInitSizes enum, updated the list of stacks, converted player_num to us players enum, and stack_name to use stacks. reduced calls on fields to use properties instead. Added the property Cards (possibly in the wrong place because some stacks don't use them). changed draw_top_card to return a Transferred_card

--- cardstack.py
+++ cardstack.py
@@ -1,51 +1,40 @@
 from enum import Enum
 
-from card import Card, Transferred_Card
+from card import Card, TransferredCard
+from player import Players
 
 class Stacks(Enum):
-    CRAPETTE = "Crapette"
-    REMAINDER = "Remainder"
-    BIN = "Bin"
-    TABLEAU = "Tableau"
-    FOUNDATION = "Foundation"
+    """The various stacks in the game."""
+    DECK:str = "Deck"
+    CRAPETTE:str = "Crapette"
+    REMAINDER:str = "Remainder"
+    BIN:str = "Bin"
+    TABLEAU_STACK:str = "Tableau Stack"
+    FOUNDATION_STACK:str = "Foundation Stack"
+    # The below do not contain cards, only stacks of cards
+    TABLEAU:str = "Tableau"
+    FOUNDATION:str = "Foundation"
+
+class StacksInitSizes(Enum):
+    """The number of cards in each stack at the start of the initialised game."""
+    DECK:int = 52
+    CRAPETTE: int = 13
+    REMAINDER: int = 35
+    BIN: int = 0
+    TABLEAU_STACK: int = 1
+    FOUNDATION_STACK:int = 0
+    # The below do not contain cards, only stacks of cards
+    TABLEAU: int = 0
+    FOUNDATION: int = 0
+
 
 
 class CardStack:
     _cards: list[Card]
-    _stack_name: str = "Parent Stack Class"
-    _player_num: int = 0 # 0 = Cartstack player number (undefined player)
+    _stack_name: Stacks
+    _player_num: Players
     
-    @property
-    def size(self) -> int:
-        return len(self._cards)
     
-    @property
-    def what_stack_am_i(self):
-        return self._stack_name
-    
-    @property
-    def top_card(self) -> Card:
-        return self._cards[len(self._cards) - 1]
-    
-    @property
-    def second_top_card(self) -> Card:
-        return self._cards[len(self._cards) - 2]
-
-    @property
-    def is_empty(self) -> bool:
-        return len(self._cards) == 0
-    
-    @property
-    def is_player(self) -> int:
-        return self._player_num
-
-    # This method contains the specific stack's rules for adding a card
-    # So needs to be overridden
-    def can_be_added(self, card: Transferred_Card) -> bool:
-        print("The method 'can_be_added' has not yet been overridden, but it must be")
-        print(card._card, card._from_player, card._from_stack_name, card._from_stack_owner)
-        return False
-
     @property
     def cards(self) -> list[Card]:
         """Return the list of cards in the Stack
@@ -54,13 +43,44 @@ class CardStack:
             list[Card]: the list of cards
         """
         return self._cards
+    
+    @property
+    def size(self) -> int:
+        return len(self.cards)
+    
+    @property
+    def what_stack_am_i(self) -> Stacks:
+        return self._stack_name
+    
+    @property
+    def top_card(self) -> Card:
+        return self.cards[self.size - 1]
+    
+    @property
+    def second_top_card(self) -> Card:
+        return self.cards[self.size - 2]
 
-    def add_card(self, card: Transferred_Card) -> bool:
-        if self.can_be_added(card):
-            self._cards.append(card._card)
+    @property
+    def is_empty(self) -> bool:
+        return self.size == 0
+    
+    @property
+    def is_player(self) -> Players:
+        return self._player_num
+
+    # The "can_be_added" method contains the specific stack's rules for adding a card
+    # So needs to be overridden
+    def can_be_added(self, transferred_card: TransferredCard) -> bool:
+        print("The method 'can_be_added' has not yet been overridden, but it must be.")
+        print(f"{transferred_card.card=}, {transferred_card.from_player=}, {transferred_card.from_stack_name=}, {transferred_card.from_stack_owner=}")
+        return False
+
+    def add_card(self, transferred_card: TransferredCard) -> bool:
+        if self.can_be_added(transferred_card):
+            self._cards.append(transferred_card.card)
             return True
         return False
-    
+     
     def force_add_card(self, card: Card) -> None:
         """Force add a card, for debug purpose
 
@@ -76,19 +96,21 @@ class CardStack:
         Returns:
             bool: True if the card was removed, False if no card was removed
         """
-        if len(self._cards) == 0:
+        if self.is_empty:
             return False
-        else:
-            try:
-                self._cards.pop()
-            except Exception as e:
-                print(f"Error {e} removing a card from the {self._stack_name} stack")
-                return False
-            if len(self._cards) > 0:
-                self._cards[len(self._cards) - 1].turn_face_up()
-            return True
         
-    def draw_top_card(self, drawing_player: int) -> Transferred_Card | None:
+        try:
+            self._cards.pop()
+        except Exception as e:
+            print(f"Error {e} removing a card from the {self.what_stack_am_i} stack")
+            return False
+        
+        if self.size > 0:
+            self.top_card.turn_face_up()
+            
+        return True
+        
+    def draw_top_card(self, drawing_player: Players) -> TransferredCard | None:
         """Draw the top card from the pack (but leaves it there)
         if the pile is empty or there is a problem drawing out a card, returns None.
         The drawn card is left on the crapette stack and will need to be removed with remove_card once it has been placed on another stack
@@ -97,17 +119,18 @@ class CardStack:
             Card: the top card if the card could be drawn, otherwise none
         """
         if self.is_empty:
-            print(f"No card on {self._stack_name} stack")
+            print(f"No card on {self.what_stack_am_i} stack")
             return None
         
         try:
-            drawn_card: Transferred_Card = Transferred_Card()
-            drawn_card._from_player = drawing_player
-            drawn_card._from_stack_name = self._stack_name
-            drawn_card._from_stack_owner = self._player_num
-            drawn_card._card = self._cards[len(self._cards) - 1]
+            drawn_card: TransferredCard = TransferredCard(#= TransferredCard()
+            from_player = drawing_player,
+            from_stack_name = self.what_stack_am_i,
+            from_stack_owner = self._player_num,
+            card = self._cards[len(self._cards) - 1]
             # drawn_card:Card = self._cards[len(self._cards) - 1]
             # self._drawn_card = drawn_card
+            )
 
         except Exception as e:
             print(f"error {e}: Could not draw card from {self._stack_name} stack")
