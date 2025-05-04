7f5707c06deeb6c59a4585d25ec012b7032cb846
Squizzy <crazy_squirrel@yahoo.com>
2025-05-04 19:56:46 +0100

    converted to use Players enum

--- pytests/tableaustack_test.py
+++ pytests/tableaustack_test.py
@@ -2,15 +2,17 @@ import sys
 sys.path.append('..')
 sys.path.append('.')
 
+from icecream import ic #type:ignore
+
 from card import Card #, Rank, Suit
 from deck import Deck
-from icecream import ic #type:ignore
+from player import Players
 
 from samples import card_8H, card_9H, card_10H, card_7D, card_7S, card_8S, card_9S, card_QS
 # import pytest
 
 from tableaustack import TableauStack
-player_num: int = 1
+# player_num: int = 1
 
 # # A set of sample cards
 # card_8H: Card = Card(rank=Rank.EIGHT, suit=Suit.HEARTS, player_num=1)
@@ -26,13 +28,13 @@ player_num: int = 1
 # card_QS: Card = Card(rank=Rank.QUEEN, suit=Suit.SPADES, player_num=1)
 
 # One deck
-deck: Deck = Deck(1)
+deck: Deck = Deck(Players.PLAYER1)
 
 # Normal initialised deck
-tableau: TableauStack = TableauStack(deck, player_num)
+tableau: TableauStack = TableauStack(deck, player_num=Players.PLAYER1)
 
 # Deck initialised with some cards
-tableau1: TableauStack = TableauStack(deck, player_num)
+tableau1: TableauStack = TableauStack(deck, player_num=Players.PLAYER1)
 tableau1.remove_top_card()
 tableau1.force_add_card(card_8S)
 tableau1.force_add_card(card_9H)
@@ -62,18 +64,18 @@ def test_top_card():
 
 def test_tableau_adder_rules():
     tableau.force_add_card(card_8H)
-    assert not tableau.can_be_added(card_8S, player_num) # same rank, should fail
-    assert not tableau.can_be_added(card_9H, player_num) # same suit, should fail
-    assert not tableau.can_be_added(card_QS, player_num) # rank far away, should fail
-    assert not tableau.can_be_added(card_9S, player_num) # rank above, should fail
-    assert not tableau.can_be_added(card_7D, player_num) # rank below but same colour, should fail
-    assert tableau.can_be_added(card_7S, player_num) # rank below, opposite colour, should pass
+    assert not tableau.can_be_added(card_8S, player_num=Players.PLAYER1) # same rank, should fail
+    assert not tableau.can_be_added(card_9H, player_num=Players.PLAYER1) # same suit, should fail
+    assert not tableau.can_be_added(card_QS, player_num=Players.PLAYER1) # rank far away, should fail
+    assert not tableau.can_be_added(card_9S, player_num=Players.PLAYER1) # rank above, should fail
+    assert not tableau.can_be_added(card_7D, player_num=Players.PLAYER1) # rank below but same colour, should fail
+    assert tableau.can_be_added(card_7S, player_num=Players.PLAYER1) # rank below, opposite colour, should pass
 
 def test_add_card():
     # top card is 8H
-    assert not tableau.add_card(card_7D, player_num)
-    assert not tableau.add_card(card_9H, player_num)
-    assert tableau.add_card(card_7S, player_num)
+    assert not tableau.add_card(card_7D, player_num=Players.PLAYER1)
+    assert not tableau.add_card(card_9H, player_num=Players.PLAYER1)
+    assert tableau.add_card(card_7S, player_num=Players.PLAYER1)
     assert tableau.top_card == card_7S
 
 def test_remove_card() -> None:
@@ -103,7 +105,7 @@ def test_deselect():
 def test_remove_cards():
     size1 = tableau1.size
     tableau1.select_cards(card_9S)
-    tableau1.remove_selected_cards()
+    tableau1.remove_selected_cards_from_stack()
     assert tableau1.size != size1
     assert tableau1.size == 3
 
@@ -119,5 +121,5 @@ def test_add_n_cards():
     ic(tableau1._selected_cards[1])
     ic(tableau1._selected_cards[2])
     # add_worked = tableau1.add_n_cards(tableau1._selected_cards)
-    assert tableau1.add_n_cards(tableau1._selected_cards, player_num)
+    assert tableau1.add_n_cards(tableau1._selected_cards, player_num=Players.PLAYER1)
     assert tableau1.size == 7
