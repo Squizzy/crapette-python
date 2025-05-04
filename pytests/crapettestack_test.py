e922baacf530823cfe62b31bc037cd2b96843bdb
Squizzy <crazy_squirrel@yahoo.com>
2025-05-04 19:58:14 +0100

    converted to use Players enum

--- pytests/crapettestack_test.py
+++ pytests/crapettestack_test.py
@@ -4,17 +4,21 @@ sys.path.append('.')
 
 # from card import Card #, Rank, Suit
 from deck import Deck
+from player import Players
+
 from samples import card_8H, card_9H, card_10H, card_7D, card_7S, card_7H
+
+
 from icecream import ic #type:ignore
 
 
 from crapettestack import CrapetteStack
-player_num:int = 1
+
 
 # One deck - reset the deck size as 2 cards were taken for the tableaux above
-deck: Deck = Deck(1)
+deck: Deck = Deck(Players.PLAYER1)
 
-crapette: CrapetteStack = CrapetteStack(deck, player_num)
+crapette: CrapetteStack = CrapetteStack(deck, Players.PLAYER1)
 
 def test_constructor():
     print()
@@ -29,20 +33,20 @@ def test_can_be_added():
     force_add_card_to_crapette()
     assert crapette.top_card == card_8H
     assert crapette.size == 14
-    assert crapette.can_be_added(card_9H, player_num)
-    assert crapette.can_be_added(card_7H, player_num)
-    assert not crapette.can_be_added(card_7D, player_num)
-    assert not crapette.can_be_added(card_10H, player_num)
-    assert not crapette.can_be_added(card_7S, player_num)
+    assert crapette.can_be_added(card_9H, Players.PLAYER1)
+    assert crapette.can_be_added(card_7H, Players.PLAYER1)
+    assert not crapette.can_be_added(card_7D, Players.PLAYER1)
+    assert not crapette.can_be_added(card_10H, Players.PLAYER1)
+    assert not crapette.can_be_added(card_7S, Players.PLAYER1)
     
 
 def test_add_card():
     assert crapette.top_card == card_8H
     assert crapette.size == 14
-    crapette.add_card(card_9H, player_num)
+    crapette.add_card(card_9H, Players.PLAYER1)
     assert crapette.top_card == card_9H
     assert crapette.size == 15
-    crapette.add_card(card_7D, player_num)
+    crapette.add_card(card_7D, Players.PLAYER1)
 
 
 def force_add_card_to_crapette():
