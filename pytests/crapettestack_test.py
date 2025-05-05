b6ab0a12c36b5ca273ac6bc517b6beaa435f80c2
Squizzy <crazy_squirrel@yahoo.com>
2025-05-05 01:29:00 +0100

    fixed missing correct use of Stacks, and made use of sample transferred_card

--- pytests/crapettestack_test.py
+++ pytests/crapettestack_test.py
@@ -5,8 +5,13 @@ sys.path.append('.')
 # from card import Card #, Rank, Suit
 from deck import Deck
 from player import Players
+from stacks import Stacks
 
-from samples import card_8H, card_9H, card_10H, card_7D, card_7S, card_7H
+from samples import card_8H, card_9H, card_10H, card_7D, card_7S, card_7H 
+from samples import card_7H_transferred_1c1, card_7H_transferred_1c2, card_7H_transferred_2c1, card_7H_transferred_1t1, card_7H_transferred_2t1
+from samples import card_9H_transferred_1c1, card_10H_transferred_1c1
+from samples import card_7D_transferred_1c1
+from samples import card_7S_transferred_1c1
 
 
 from icecream import ic #type:ignore
@@ -25,7 +30,7 @@ def test_constructor():
     ic(crapette.what_stack_am_i)
     ic(crapette.top_card)
     
-    assert crapette.what_stack_am_i == "Crapette"
+    assert crapette.what_stack_am_i == Stacks.CRAPETTE
     assert crapette.size == 13
     assert crapette.top_card.face_up
 
@@ -33,20 +38,20 @@ def test_can_be_added():
     force_add_card_to_crapette()
     assert crapette.top_card == card_8H
     assert crapette.size == 14
-    assert crapette.can_be_added(card_9H, Players.PLAYER1)
-    assert crapette.can_be_added(card_7H, Players.PLAYER1)
-    assert not crapette.can_be_added(card_7D, Players.PLAYER1)
-    assert not crapette.can_be_added(card_10H, Players.PLAYER1)
-    assert not crapette.can_be_added(card_7S, Players.PLAYER1)
+    assert crapette.can_be_added(card_9H_transferred_1c1)
+    assert crapette.can_be_added(card_7H_transferred_1c1)
+    assert not crapette.can_be_added(card_7D_transferred_1c1)
+    assert not crapette.can_be_added(card_10H_transferred_1c1)
+    assert not crapette.can_be_added(card_7S_transferred_1c1)
     
 
 def test_add_card():
     assert crapette.top_card == card_8H
     assert crapette.size == 14
-    crapette.add_card(card_9H, Players.PLAYER1)
-    assert crapette.top_card == card_9H
+    crapette.add_card(card_7H_transferred_1c1) # should work
+    assert crapette.top_card == card_7H ##
     assert crapette.size == 15
-    crapette.add_card(card_7D, Players.PLAYER1)
+    crapette.add_card(card_7D_transferred_1c1)
 
 
 def force_add_card_to_crapette():
