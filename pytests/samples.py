b7e5c1d000050c4559403465f3f2e20495f5ed6f
Squizzy <crazy_squirrel@yahoo.com>
2025-05-05 01:27:47 +0100

    added some transferred_card samples

--- pytests/samples.py
+++ pytests/samples.py
@@ -1,4 +1,5 @@
-from card import Card, Rank, Suit
+from card import Card, Rank, Suit, TransferredCard, TransferredNCards
+from stacks import Stacks
 from deck import Deck
 from tableaustack import TableauStack
 from player import Players
@@ -24,6 +25,28 @@ card_QS: Card = Card(rank=Rank.QUEEN, suit=Suit.SPADES, player_num=Players.PLAYE
 random_stacked_cards: list[Card] = [card_7S, card_8S, card_9S, card_QS, card_9S, card_7D, card_7S]
 correct_stacked_cards_down_low: list[Card]   = [card_7S, card_8H, card_9S]
 correct_stacked_cards_down_high: list[Card]  = [card_10H, card_JS]
+
+# A set of sample Transferred_Card
+card_7H_transferred_1t1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.TABLEAU_STACK, from_stack_owner=Players.PLAYER1, card=card_7H)
+card_7H_transferred_2t1: TransferredCard = TransferredCard(from_player=Players.PLAYER2, from_stack_name=Stacks.TABLEAU_STACK, from_stack_owner=Players.PLAYER1, card=card_7H)
+card_7H_transferred_1c1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.CRAPETTE, from_stack_owner=Players.PLAYER1, card=card_7H)
+card_7H_transferred_1c2: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.CRAPETTE, from_stack_owner=Players.PLAYER2, card=card_7H)
+card_7H_transferred_2c1: TransferredCard = TransferredCard(from_player=Players.PLAYER2, from_stack_name=Stacks.CRAPETTE, from_stack_owner=Players.PLAYER1, card=card_7H)
+
+card_8H_transferred_1t1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.TABLEAU_STACK, from_stack_owner=Players.PLAYER1, card=card_8H)
+card_9H_transferred_1c1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.CRAPETTE, from_stack_owner=Players.PLAYER1, card=card_9H)
+card_10H_transferred_1c1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.CRAPETTE, from_stack_owner=Players.PLAYER1, card=card_10H)
+
+card_7D_transferred_1c1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.CRAPETTE, from_stack_owner=Players.PLAYER1, card=card_7D)
+
+card_7S_transferred_1c1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.CRAPETTE, from_stack_owner=Players.PLAYER1, card=card_7S)
+card_7S_transferred_1t1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.TABLEAU_STACK, from_stack_owner=Players.PLAYER1, card=card_7S)
+card_8S_transferred_1c1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.CRAPETTE, from_stack_owner=Players.PLAYER1, card=card_8S)
+card_9S_transferred_1t1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.TABLEAU_STACK, from_stack_owner=Players.PLAYER1, card=card_9S)
+card_QS_transferred_1c1: TransferredCard = TransferredCard(from_player=Players.PLAYER1, from_stack_name=Stacks.CRAPETTE, from_stack_owner=Players.PLAYER1, card=card_QS)
+
+# A set of sample TransferredNCards
+transferredNCards_9S_8H_7S: TransferredNCards = TransferredNCards(from_player=Players.PLAYER1, from_stack_name=Stacks.TABLEAU_STACK, from_stack_owner=Players.PLAYER1, cards=[card_9S, card_8H, card_7S])
 
 # One deck
 tableaux_deck: Deck = Deck(playerNum=Players.PLAYER1)
