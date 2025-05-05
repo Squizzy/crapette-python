a182987f0bf3f9f06d069269267d863851b8b77f
Squizzy <crazy_squirrel@yahoo.com>
2025-05-05 03:19:16 +0100

    minor changes

--- pytests/crapettestack_test.py
+++ pytests/crapettestack_test.py
@@ -2,22 +2,21 @@ import sys
 sys.path.append('..')
 sys.path.append('.')
 
+from icecream import ic #type:ignore
+
 # from card import Card #, Rank, Suit
 from deck import Deck
 from player import Players
 from stacks import Stacks
+from crapettestack import CrapetteStack
 
-from samples import card_8H, card_9H, card_10H, card_7D, card_7S, card_7H 
+from samples import card_7H, card_8H, card_9H, card_10H
+from samples import card_7D
+from samples import card_7S
 from samples import card_7H_transferred_1c1, card_7H_transferred_1c2, card_7H_transferred_2c1, card_7H_transferred_1t1, card_7H_transferred_2t1
 from samples import card_9H_transferred_1c1, card_10H_transferred_1c1
 from samples import card_7D_transferred_1c1
 from samples import card_7S_transferred_1c1
-
-
-from icecream import ic #type:ignore
-
-
-from crapettestack import CrapetteStack
 
 
 # One deck - reset the deck size as 2 cards were taken for the tableaux above
