9405b071c0dd7129a03795a792071b2c851d9c81
Squizzy <crazy_squirrel@yahoo.com>
2025-05-05 01:27:07 +0100

    updated to use the new Stacks enum module. and added the initialisation of the stack name

--- tableaustacks.py
+++ tableaustacks.py
@@ -1,7 +1,7 @@
 from deck import Deck
 from tableaustack import TableauStack
 from player import Players
-from cardstack import Stacks, StacksInitSizes
+from stacks import Stacks, StacksInitSizes
 
 class TableauStacks:
     _stack_name: Stacks
@@ -11,7 +11,8 @@ class TableauStacks:
     def __init__(self, deck:Deck, player_num: Players):
         if player_num not in [Players.PLAYER1, Players.PLAYER2]:
             raise ValueError(f"Error: Problem initiating Tableau stacks - player specified incorrect: {player_num}")
-        self._player_num =  player_num
+        self._stack_name = Stacks.TABLEAU_STACK  # Name of the stack
+        self._player_num =  player_num  # Player number of the stack owner
         self._TableauStacks = []
         for _ in range(StacksInitSizes.TABLEAU.value):
             tableau_stack = TableauStack(deck, player_num)
