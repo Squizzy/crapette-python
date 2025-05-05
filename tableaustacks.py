5a36b41fb786282668c0fa2934ab25eb26688c44
Squizzy <crazy_squirrel@yahoo.com>
2025-05-05 14:32:53 +0100

    bug fix (stack name)

--- tableaustacks.py
+++ tableaustacks.py
@@ -13,7 +13,7 @@ class TableauStacks:
     def __init__(self, deck:Deck, player_num: Players):
         if player_num not in [Players.PLAYER1, Players.PLAYER2]:
             raise ValueError(f"Error: Problem initiating Tableau stacks - player specified incorrect: {player_num}")
-        self._stack_name = Stacks.TABLEAU_STACK  # Name of the stack
+        self._stack_name = Stacks.TABLEAU  # Name of the stack
         self._player_num =  player_num  # Player number of the stack owner
         self._TableauStacks = []
         for _ in range(StacksInitSizes.TABLEAU.value):
