fca15df57a3566d9e34b40772a58e3a48d2bdc84
Squizzy <crazy_squirrel@yahoo.com>
2025-06-05 15:41:52 +0100

    Refactored the StacksPositions class making it easier to resize the whole game; created an initial GameState class

--- pygame-client.py
+++ pygame-client.py
@@ -25,87 +25,156 @@ card_game_height: int = int(card_scale_landscape)
 card_game_width: int = int(card_img_width * card_game_height / card_img_height)
 
 
-class StacksPositions:
-    _x: int
-    _y: int
-    # _name: str
-
-    def __init__(self, x: int, y: int) -> None:
-        if  x < 0 or \
-            y < 0 or \
-            x > (GAME_WIDTH - card_game_width) or\
-            y > (GAME_HEIGHT - card_game_height):
-            raise ValueError("The stack is not being positioned on the game area")
-        self._x = x
-        self._y = y
-
-    @property
-    def x(self) -> int:
-        return self._x
+# Stores a copy of the game state from the server
+# Updates from the server
+class GameState:
+    _stacks: dict[str, list[str]]
+    _turn_player: int
     
-    @property
-    def y(self) -> int:
-        return self._y
+    def __init__(self) -> None:
+        self._stacks = {
+            "player_crapette":      [],
+            "player_remainder":     [],
+            "player_bin":           [],
+            "player_tableau":       [],
+            "player_foundation":    [],
+            "opponent_crapette":    [],
+            "opponent_remainder":   [],
+            "opponent_bin":         [],
+            "opponent_tableau":     [],
+            "opponent_foundation":  [],
+        }
+        
 
-# vertical alignments
-OPPONENT_ROW: int = int(card_game_height / 7)
-
-TABLEAU_ROW_1: int = OPPONENT_ROW + card_game_height + int(card_game_height / 7)
-TABLEAU_ROW_2: int = TABLEAU_ROW_1 + card_game_height + int(card_game_height / (7 + 7/3))
-TABLEAU_ROW_3: int = TABLEAU_ROW_2 + card_game_height + int(card_game_height / (7 + 7/3))
-TABLEAU_ROW_4: int = TABLEAU_ROW_3 + card_game_height + int(card_game_height / (7 + 7/3))
-
-FOUNDATION_ROW_1: int = TABLEAU_ROW_1 + int((card_game_height - card_game_width) /2)
-FOUNDATION_ROW_2: int = TABLEAU_ROW_2 + int((card_game_height - card_game_width) /2)
-FOUNDATION_ROW_3: int = TABLEAU_ROW_3 + int((card_game_height - card_game_width) /2)
-FOUNDATION_ROW_4: int = TABLEAU_ROW_4 + int((card_game_height - card_game_width) /2)
-
-PLAYER_ROW: int = TABLEAU_ROW_4 + card_game_height + int(card_game_height / 7)
-
-# horizontal alignments
-REMAINDER_OPPONENT_COL: int = int(GAME_WIDTH / 2) - int(card_game_width / 2)
-BIN_OPPONENT_COL: int = REMAINDER_OPPONENT_COL - int(card_game_width / 2) - card_game_width
-CRAPETTE_OPPONENT_COL: int = REMAINDER_OPPONENT_COL + card_game_width + int(card_game_width / 2)
-
-CRAPETTE_PLAYER_COL: int = BIN_OPPONENT_COL
-REMAINDER_PLAYER_COL: int = REMAINDER_OPPONENT_COL
-BIN_PLAYER_COL: int = CRAPETTE_OPPONENT_COL
-
-FOUNDATION_PLAYER_COL: int = int(GAME_WIDTH / 2) + int(card_game_width / 4)
-TABLEAU_PLAYER_COL: int = int(GAME_WIDTH / 2) + card_game_height + int(card_game_width / 2)
-FOUNDATION_OPPONENT_COL: int = int(GAME_WIDTH / 2) - int(card_game_width / 4) - card_game_height
-TABLEAU_OPPONENT_COL: int = int(GAME_WIDTH / 2) - card_game_height - int(card_game_width / 2) - card_game_width
-
-# stack_positions: name: (x, y), vertical (true) or horizontal
-stacks_positions: dict[str, tuple[int, int, bool]] = {
-    "opponent_crapette":    (CRAPETTE_OPPONENT_COL,  OPPONENT_ROW, True),
-    "opponent_remainder":   (REMAINDER_OPPONENT_COL, OPPONENT_ROW, True),
-    "opponent_bin":         (BIN_OPPONENT_COL,       OPPONENT_ROW, True),
-
-    "opponent_tableau_4":   (TABLEAU_OPPONENT_COL, TABLEAU_ROW_4, True),
-    "opponent_tableau_3":   (TABLEAU_OPPONENT_COL, TABLEAU_ROW_3, True),
-    "opponent_tableau_2":   (TABLEAU_OPPONENT_COL, TABLEAU_ROW_2, True),
-    "opponent_tableau_1":   (TABLEAU_OPPONENT_COL, TABLEAU_ROW_1, True),
+class StacksLayout:
+    _screen_width: int
+    _screen_height: int
+    _card_width: int
+    _card_height: int
+    _margin_x: int
+    _margin_y: int
+    _center_x: int
+    _center_y: int
     
-    "opponent_foundation_4": (FOUNDATION_OPPONENT_COL, FOUNDATION_ROW_4, False),
-    "opponent_foundation_3": (FOUNDATION_OPPONENT_COL, FOUNDATION_ROW_3, False),
-    "opponent_foundation_2": (FOUNDATION_OPPONENT_COL, FOUNDATION_ROW_2, False),
-    "opponent_foundation_1": (FOUNDATION_OPPONENT_COL, FOUNDATION_ROW_1, False),
+    def __init__(self) -> None:
+        """
+        Initialise the stacks layout
+        Calculate the positions of the stacks
+        Store the positions in the _positions attribute
+        """
+        self._screen_width = GAME_WIDTH
+        self._screen_height = GAME_HEIGHT
+        self._card_width = card_game_width
+        self._card_height = card_game_height
+        
+        self._margin_x = self._card_width // 7
+        self._margin_y = self._card_height // 7
+        self._center_x = self._screen_width // 2
+        self._center_y = self._screen_height // 2
+                
+        self._positions = self._calculate_positions()
+        
+    @property
+    def stacks_positions(self) -> dict[str, tuple[int, int, bool]]:
+        """ 
+        Get the positions of the stacks
+        
+        Returns:
+            dict[str, tuple[int, int, bool]]: a dictionary containing:
+                - key: the name of the stack
+                - values:
+                    - the top left corner coordinate of the bottom of the stack x, y
+                    - a boolean indicating if the card is placed vertically or horizontally
+        """
+        return self._positions
+    
+    def _calculate_positions(self) -> dict[str, tuple[int, int, bool]]:
+        """
+        Calculate the positions of the stacks
+        Assign the top left corner coordiinate of the stacks
+        Assign the orientation of the cards in the stacks
 
-    "player_foundation_1": (FOUNDATION_PLAYER_COL, FOUNDATION_ROW_4, False),
-    "player_foundation_2": (FOUNDATION_PLAYER_COL, FOUNDATION_ROW_3, False),
-    "player_foundation_3": (FOUNDATION_PLAYER_COL, FOUNDATION_ROW_2, False),
-    "player_foundation_4": (FOUNDATION_PLAYER_COL, FOUNDATION_ROW_1, False),
+        Returns:
+            dict[str, tuple[int, int, bool]]: a dictionary containing:
+                - the name of the stack
+                - the top left corner coordinate of the bottom of the stack
+                - a boolean indicating if the card is placed vertically or horizontally
+            
+        """
+        # x positions (top left corner)
+        player_center_x = self._center_x - self._card_width // 2
+        player_left_x = player_center_x - self._card_width - self._margin_x
+        player_right_x = player_center_x + self._card_width + self._margin_x
+        player_foundation_x = self._center_x + self._margin_x
+        player_tableau_x = player_foundation_x + self._margin_x + self._card_height
+        
+        opponent_center_x = self._center_x - self._card_width // 2
+        opponent_left_x = opponent_center_x - self._card_width - self._margin_x
+        opponent_right_x = opponent_center_x + self._card_width + self._margin_x
+        opponent_foundation_x = self._center_x - self._margin_x - self._card_height
+        opponent_tableau_x = opponent_foundation_x - self._margin_x - self._card_width
+        
+        # y positions (top left corner)
+        opponent_y = self._center_y - self._card_height * 3 - int(self._margin_y * 2.5)
+        player_y = self._center_y + self._card_height * 2 + int(self._margin_y * 2.5)
+        tableau_top_y = self._center_y - self._card_height * 2 - int(self._margin_y * 1.5)
+        foundation_top_y = tableau_top_y + (self._card_height - self._card_width) //2
+        tableau_spacing_y = self._card_height + self._margin_y
+        # foundation_spacing_y = tableau_spacing_y + self._margin_y
+        
+        opponent_base_stacks_positions: dict[str, tuple[int, int, bool]] = {
+            "opponent_crapette":    (opponent_right_x,  opponent_y, True),
+            "opponent_remainder":   (opponent_center_x, opponent_y, True),
+            "opponent_bin":         (opponent_left_x, opponent_y, True),
+        }
 
-    "player_tableau_1": (TABLEAU_PLAYER_COL, TABLEAU_ROW_4, True),
-    "player_tableau_2": (TABLEAU_PLAYER_COL, TABLEAU_ROW_3, True),
-    "player_tableau_3": (TABLEAU_PLAYER_COL, TABLEAU_ROW_2, True),
-    "player_tableau_4": (TABLEAU_PLAYER_COL, TABLEAU_ROW_1, True),
+        player_base_stacks_positions: dict[str, tuple[int, int, bool]] = {
+            "player_crapette":  (player_left_x, player_y, True),
+            "player_remainder": (player_center_x, player_y, True),
+            "player_bin":       (player_right_x, player_y, True),
+        }
+        
+        center_stacks_positions: dict[str, tuple[int, int, bool]] = {}
+        for p in range(4):
+            o = 3 - p
+            center_stacks_positions[f"opponent_tableau_{o}"] = (opponent_tableau_x, tableau_top_y + tableau_spacing_y * o, True)
+            center_stacks_positions[f"opponent_foundation_{o}"] = (opponent_foundation_x, foundation_top_y + tableau_spacing_y * o, False)
+            center_stacks_positions[f"player_tableau_{p}"] = (player_tableau_x, tableau_top_y + tableau_spacing_y * p, True)
+            center_stacks_positions[f"player_foundation_{p}"] = (player_foundation_x, foundation_top_y + tableau_spacing_y * p, False)
 
-    "player_crapette":  (CRAPETTE_PLAYER_COL,  PLAYER_ROW, True),
-    "player_remainder": (REMAINDER_PLAYER_COL, PLAYER_ROW, True),
-    "player_bin":       (BIN_PLAYER_COL,       PLAYER_ROW, True),
-}
+    #             f"opponent_tableau_4":   (opponent_tableau_x, tableau_top_y, True),
+    #             f"opponent_tableau_3":   (opponent_tableau_x, tableau_top_y + tableau_spacing_y, True),
+    #             f"opponent_tableau_2":   (opponent_tableau_x, tableau_top_y + tableau_spacing_y * 2, True),
+    #             f"opponent_tableau_1":   (opponent_tableau_x, tableau_top_y + tableau_spacing_y * 3, True),
+    #             f
+    #             f"opponent_foundation_4": (opponent_foundation_x, foundation_top_y, False),
+    #             f"opponent_foundation_3": (opponent_foundation_x, foundation_top_y + foundation_spacing_y, False),
+    #             f"opponent_foundation_2": (opponent_foundation_x, foundation_top_y + foundation_spacing_y * 2, False),
+    #             f"opponent_foundation_1": (opponent_foundation_x, foundation_top_y + foundation_spacing_y * 3, False),
+    # f
+    #             f"player_foundation_1": (player_foundation_x, foundation_top_y, False),
+    #             f"player_foundation_2": (player_foundation_x, foundation_top_y + foundation_spacing_y, False),
+    #             f"player_foundation_3": (player_foundation_x, foundation_top_y + foundation_spacing_y * 2, False),
+    #             f"player_foundation_4": (player_foundation_x, foundation_top_y + foundation_spacing_y * 3, False),
+    # f
+    #             f"player_tableau_1": (player_tableau_x, tableau_top_y, True),
+    #             f"player_tableau_2": (player_tableau_x, tableau_top_y + tableau_spacing_y, True),
+    #             f"player_tableau_3": (player_tableau_x, tableau_top_y + tableau_spacing_y * 2, True),
+    #             f"player_tableau_4": (player_tableau_x, tableau_top_y + tableau_spacing_y * 3, True),
+
+        # }
+        # combine the 3 dictionaries
+        stacks_positions = opponent_base_stacks_positions |center_stacks_positions | player_base_stacks_positions    
+        return stacks_positions
+    
+    def screen_resize(self, width: int, height: int):
+        """
+        Resize the screen
+        Recalculate the positions of the stacks
+        """
+        self._screen_width = width
+        self._screen_height = height
+        self._calculate_positions()
 
 
 card_faces: dict[str, pygame.Surface] = {}
@@ -119,7 +188,7 @@ def pygame_init():
 
 def window_init() -> pygame.Surface:
     # Set up the game window
-    surface: pygame.Surface = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT)) #, pygame.RESIZABLE)
+    surface: pygame.Surface = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT), pygame.RESIZABLE)
 
     # Set title of window
     pygame.display.set_caption("pygame title")
@@ -183,6 +252,8 @@ def load_card_faces():
 
 
 def place_stacks(surface: pygame.Surface):
+    stacks_positions = StacksLayout().stacks_positions
+    
     for stack in stacks_positions:
         blit_blank = card_faces["EC"] if stacks_positions[stack][2] \
             else pygame.transform.rotate(card_faces["EC"].copy(), 90)
@@ -227,6 +298,9 @@ def game_loop(surface: pygame.Surface):
             elif event.type == pygame.MOUSEMOTION and moving:
                 # print(f"Mouse Move: {rect.x=}, {rect.y=}")
                 rect.move_ip(event.rel)
+            elif  event.type == pygame.VIDEORESIZE:
+                # print(f"Video Resize: {event.size=}")
+                StacksLayout().screen_resize(event.w, event.h)
             else:
                 pass
         
