2b74f8d67add1e4fea79560663289cd10e829117
Squizzy <crazy_squirrel@yahoo.com>
2025-06-05 13:38:53 +0100

    moved to using icecrean in a dedicated log_message function

--- client_network.py
+++ client_network.py
@@ -1,6 +1,14 @@
 import socket
 import time
 from icecream import ic # type: ignore
+
+# enable debug messages
+DEBUG = True
+
+def log_message(message: str):
+    if DEBUG:
+        ic(f"pygame-client: {message}")
+
 class ClientConnection:
     # _client_ip: str
     # _client_port: int
@@ -17,27 +25,42 @@ class ClientConnection:
         self._connected = False
         self._client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        ic("client socket initialised")
+        log_message("client socket initialised")
 
     def __del__(self):
         self._client_socket.close()
-        ic("client socket closed")
+        log_message("client socket closed")
 
-    def connect(self):
-        ic("client connecting to server")
-        self._client_socket.connect((self._server_ip, self._server_port))
-        # self._recv_connection_ack()
-        ic("client waiting for connection ack")
-        ack:bool | None = self._receive_ack("connection accepted")
-        if ack:
-            ic("client connected")
-            self._connected = True
-        elif ack is False:
-            ic("client not connected")
-            self._connected = False
-        else:
-            print("wrong ack received")
-        ic("client connection ack received")
+    def connect(self) -> bool:
+        """Connect to the server
+        Send a connection request to the server 
+        wait for an ack from the server 
+        
+        Returns:
+            True if the connection is successful, False otherwise.
+        """
+        
+        log_message("client connecting to server")
+        
+        if self._connected:
+            log_message("client already connected")
+
+        else:        
+            self._client_socket.connect((self._server_ip, self._server_port))
+            # self._recv_connection_ack()
+            log_message("client waiting for connection ack")
+            ack:bool | None = self._receive_ack("connection accepted")
+            if ack:
+                log_message("client connected")
+                self._connected = True
+            elif ack is False:
+                log_message("client not connected")
+                self._connected = False
+            else:
+                print("wrong ack received")
+            log_message("client connection ack received")
+            
+        return self._connected
     # def _recv_connection_ack(self):
     #     data = self._recv(1024)
     #     if data == b"connected":
@@ -49,7 +72,7 @@ class ClientConnection:
     
     def _receive_ack(self, ack_message) -> bool | None:
         data = self._recv(1024)
-        ic(f"_receive_ack: client received ack data: {data}")
+        log_message(f"_receive_ack: client received ack data: {data}")
         answer:bool | None = None if data.decode() == "" else \
                              True if data.decode() == ack_message else \
                              False
@@ -57,18 +80,18 @@ class ClientConnection:
     
     def disconnect(self):
         self._connected = False
-        ic("_disconnect: client disconnected")
+        log_message("_disconnect: client disconnected")
         # self._client_socket.sendall(b"hello world")
         # data = self._client_socket.recv(1024)
 
         # print(f"Client received {data!r}")
 
     def _send(self, data: bytes):
-        ic("_send: client sending data")
+        log_message("_send: client sending data")
         self._client_socket.sendall(data)
 
     def _recv(self, size: int):
-        ic("_recv: client receiving data")
+        log_message("_recv: client receiving data")
         data:bytes = b""
         recvd: list[bytes] = []
         while True:
@@ -77,46 +100,49 @@ class ClientConnection:
             if data == b"":
                 break
             return data
-        ic("_recv: client received data finished")
+        log_message("_recv: client received data finished")
 
     def server_get_player_id(self) -> int | None:
-        ic("server_get_player_id: client getting player id")
+        log_message("server_get_player_id: client getting player id")
         self._send(b"get_player_id")
         data = self._recv(1024)
         if data == b"":
-            ic("no client player id received")
+            log_message("no client player id received")
             return None
         else:
             self._player_id = int(data)
-            ic(f"client player id received: {self._player_id}")
+            log_message(f"client player id received: {self._player_id}")
             return self._player_id
 
     def server_get_stacks_cards(self):
-        ic("server_get_stacks_cards: client requesting stacks cards")
+        log_message("server_get_stacks_cards: client requesting stacks cards")
         # empty the current stacks
         self._stacks_cards = []
         # set the message encoded
         message = f"get_stacks_cards {self._player_id}"
         self._send(message.encode())
+        
         # receive the stacks information until the server sends an empty message
         while True:
             data = self._recv(1024)
             if data == b"":
                 break
             self._stacks_cards.append(data)
+            
         # if the stacks cards are not empty, print the stacks cards
         if len(self._stacks_cards) > 0:
-            ic(f"client stacks cards received: {self._stacks_cards}")
+            log_message(f"client stacks cards received: {self._stacks_cards}")
         else:
-            ic("no client stacks cards received")
+            log_message("no client stacks cards received")
 
     
-    def server_quit(self):
-        ic("server_quit: client quitting")
+    def server_quit(self) -> bool:
+        log_message("server_quit: client quitting")
         self._send(b"quit")
         ack:bool | None = self._receive_ack("quit accepted")
-        ic(f"server_quit: client quit ack received: {ack}")
+        log_message(f"server_quit: client quit ack received: {ack}")
         self.disconnect()
+        return self._connected
 
 if __name__ in "__main__":
     i = 0
