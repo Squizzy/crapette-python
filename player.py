80258847b5d1a523311edfad0573f6401dd78ce9
Squizzy <crazy_squirrel@yahoo.com>
2025-06-07 14:39:55 +0100

    move the players enum out;  added the log_message debug; added some debugging info

--- player.py
+++ player.py
@@ -1,18 +1,26 @@
-from enum import Enum
+# from enum import Enum
 from deck import Deck
 from crapettestack import CrapetteStack
 from remainderstack import RemainderStack
 from binstack import BinStack
 from tableaustacks import TableauStacks
 from foundationstacks import FoundationStacks
+from constants import Players
 
-# The enum representing the players
-class Players(Enum):
-    PLAYER1 = 0,
-    PLAYER2 = 1,
-    CARDSTACK = 2 # mostly for debugging purposes for the moment
+from icecream import ic # type: ignore
+
+# # The enum representing the players
+# class Players(Enum):
+#     PLAYER1 = 0,
+#     PLAYER2 = 1,
+#     CARDSTACK = 2 # mostly for debugging purposes for the moment
 
 
+DEBUG: bool = False
+def log_message(message: str):
+    if DEBUG:
+        ic(f"player: {message}")
+        
 class Player:
     _player_num: Players
     _crapette: CrapetteStack
@@ -26,10 +34,28 @@ class Player:
         if player_num not in [Players.PLAYER1, Players.PLAYER2]:
             raise ValueError(f"Error: Problem initiating Tableau stacks - player specified incorrect: {player_num}")
         self._player_num =  player_num  # Player number of the stack owner
+        
         deck = Deck(player_num)
+        log_message(f"{deck.size=}")
+        
         self._crapette = CrapetteStack(deck, player_num)
+        log_message(f"{self._crapette.size=}")
+        log_message(f"{deck.size=}")
+
+        self._tableau = TableauStacks(deck, player_num)        
+        log_message(f"{self._tableau._TableauStacks[0].size=}")
+        log_message(f"{deck.size=}")
+
+        
         self._remainder = RemainderStack(deck, player_num)
+        log_message(f"{self._remainder.size=}")
+        log_message(f"{deck.size=}")
+
         self._bin = BinStack(player_num)
-        self._tableau = TableauStacks(deck, player_num)
+        log_message(f"{self._bin.size=}")
+        log_message(f"{deck.size=}")
+
         self._foundation = FoundationStacks(deck, player_num)
+        log_message(f"{self._foundation._FoundationStacks[0].size=}")
+        log_message(f"{deck.size=}")
         
