12f771a4387d8f5c836fc53dcf5c05b1a11380a3
Squizzy <crazy_squirrel@yahoo.com>
2025-06-07 14:30:07 +0100

    created a dedicated file for the enum to avoid circular references

---
+++ constants.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+# The enum representing the players
+class Players(Enum):
+    PLAYER1 = 0,
+    PLAYER2 = 1,
+    CARDSTACK = 2 # mostly for debugging purposes for the moment
