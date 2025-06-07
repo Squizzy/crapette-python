db675552265fc0615cc51e736db4928b31597fa9
Squizzy <crazy_squirrel@yahoo.com>
2025-06-07 14:37:13 +0100

    updated the ic output for consistency; updated the connect method as ack of connectio is not needed; updated the _recv method as recv is blocking; updated the player_id request to not use binary by default; updated the get_stack_cards to receive proper json data; simplified main; added docstrings

--- client_network.py
+++ client_network.py
@@ -1,13 +1,18 @@
 import socket
-import time
+# import time
+from constants import Players
+from json import loads
 from icecream import ic # type: ignore
 
 # enable debug messages
 DEBUG = True
 
+ic.configureOutput(prefix="client_network: ")
+
 def log_message(message: str):
     if DEBUG:
-        ic(f"pygame-client: {message}")
+        ic(message)
+        # ic(f"client_network: {message}")
 
 class ClientConnection:
     # _client_ip: str
@@ -28,16 +33,19 @@ class ClientConnection:
         log_message("client socket initialised")
 
     def __del__(self):
+        # shut down the sockets so all connections are closed
+        # self._client_socket.shutdown(socket.SHUT_RDWR)
+        # close the socket to deallocate it
         self._client_socket.close()
-        log_message("client socket closed")
+        # log_message("client socket closed")
 
-    def connect(self) -> bool:
-        """Connect to the server
+    def connect(self) -> None:
+        """
+        Connect to the server
         Send a connection request to the server 
-        wait for an ack from the server 
         
         Returns:
-            True if the connection is successful, False otherwise.
+            Nothing
         """
         
         log_message("client connecting to server")
@@ -46,115 +54,152 @@ class ClientConnection:
             log_message("client already connected")
 
         else:        
-            self._client_socket.connect((self._server_ip, self._server_port))
-            # self._recv_connection_ack()
-            log_message("client waiting for connection ack")
-            ack:bool | None = self._receive_ack("connection accepted")
-            if ack:
-                log_message("client connected")
-                self._connected = True
-            elif ack is False:
-                log_message("client not connected")
-                self._connected = False
-            else:
-                print("wrong ack received")
-            log_message("client connection ack received")
-            
-        return self._connected
-    # def _recv_connection_ack(self):
-    #     data = self._recv(1024)
-    #     if data == b"connected":
-    #         self._connected = True
-    #         ic("client connected ack received")
-    #     else:
-    #         self._connected = False
-    #         ic("client not connected ack not received")
-    
-    def _receive_ack(self, ack_message) -> bool | None:
-        data = self._recv(1024)
-        log_message(f"_receive_ack: client received ack data: {data}")
-        answer:bool | None = None if data.decode() == "" else \
-                             True if data.decode() == ack_message else \
-                             False
-        return answer
-    
+            log_message("requesting connection")
+            try:
+                self._client_socket.connect((self._server_ip, self._server_port))
+            except ConnectionError as e:
+                raise ConnectionError(f"Failed to connect to server: {e}")
+        
+        self._connected = True
+        log_message("Connected to server")
+        
     def disconnect(self):
+        """
+        Disconnect from the server
+        Probably not strictly necessary? To be checked later
+        """
+        self.__del__()
         self._connected = False
         log_message("_disconnect: client disconnected")
-        # self._client_socket.sendall(b"hello world")
-        # data = self._client_socket.recv(1024)
 
-        # print(f"Client received {data!r}")
 
-    def _send(self, data: bytes):
-        log_message("_send: client sending data")
-        self._client_socket.sendall(data)
+    def _send(self, data: str):
+        """
+        Send data to the server. 
+        Data needs to be a string (ideally json encoded)
 
-    def _recv(self, size: int):
+        Args:
+            data (str): text content to transfer
+        """
+        
+        log_message("_send: sending data")
+        self._client_socket.sendall(data.encode())
+        log_message("_send: data sent")
+
+    def _recv(self, max_buffer_size: int) -> str:
+        """
+        Receives data from the server and returns it as a string (maybe json encoded?).
+
+        Args:
+            max_buffer_size (int): maximum buffer size in bytees to receive data
+
+        Returns:
+            str: the received data in string format
+        """
         log_message("_recv: client receiving data")
-        data:bytes = b""
-        recvd: list[bytes] = []
-        while True:
-            data = self._client_socket.recv(size)
-            ic(f"_recv:client received data: {recvd}")
-            if data == b"":
-                break
-            return data
-        log_message("_recv: client received data finished")
+        data: bytes = b""
+        received_message: str = ""
+        data = self._client_socket.recv(max_buffer_size)
+        received_message = data.decode()
+        # while True:
+        #     data = self._client_socket.recv(max_buffer_size)
+        #     # ic(f"_recv:client received data: {data.decode()}")
+        #     if data == b"":
+        #         if received_message == "":
+        #             continue
+        #         else:
+        #             break
+        #     else:
+        #         received_message += data.decode()
+         
+        log_message(f"_recv: client received data finished. {len(received_message)=} received")
+        # log_message(received_message)
+        return received_message
 
-    def server_get_player_id(self) -> int | None:
-        log_message("server_get_player_id: client getting player id")
-        self._send(b"get_player_id")
-        data = self._recv(1024)
-        if data == b"":
-            log_message("no client player id received")
-            return None
-        else:
-            self._player_id = int(data)
-            log_message(f"client player id received: {self._player_id}")
-            return self._player_id
+    def request_player_id_from_server(self) -> Players:
+        """
+        find out if we are player1 or player2
+
+        Returns:
+            Players: Players.PLAYER1 or 2
+        """
+        log_message("request_player_id_from_server: sending request")
+        
+        message = "send_player_id"
+        self._send(message)
+        
+        data: str = self._recv(1024)
+        message =  loads(data)
+        
+        # if data == b"":
+        #     log_message("no client player id received")
+        #     return None
+        # else:
+        
+        #TODO: Convert to Playsers - maybe even using a dedicated method
+        print(type(data))
+        self._player_id = int(message)
+        log_message(f"client player id received: {self._player_id}")
+        return self._player_id
+
 
     def server_get_stacks_cards(self):
         log_message("server_get_stacks_cards: client requesting stacks cards")
         # empty the current stacks
         self._stacks_cards = []
         # set the message encoded
-        message = f"get_stacks_cards {self._player_id}"
-        self._send(message.encode())
-        
+        # message = f"get_stacks_cards {self._player_id}"
+        message = "get_stacks_cards"
+        # self._send(message.encode())
+        self._client_socket.sendall(message.encode())
+        log_message(f"requested {message} from server")
         # receive the stacks information until the server sends an empty message
-        while True:
-            data = self._recv(1024)
-            if data == b"":
-                break
-            self._stacks_cards.append(data)
+        data = self._recv(10000)
+        message = loads(data)
+        # data_recieved = False
+        # while True:
+        #     data = self._client_socket.recv(1024)
+        #     # data = self._recv(1024)
+        #     if data == b"":
+        #         if data_recieved:
+        #             break
+        #         else:
+        #             continue
+        #     else:
+        #         data_recieved = True
+        # print(message)
+        # self._stacks_cards.append(data.decode())
+            
+            # print(data.decode())
+            # self._stacks_cards.append(data.decode())
             
         # if the stacks cards are not empty, print the stacks cards
-        if len(self._stacks_cards) > 0:
-            log_message(f"client stacks cards received: {self._stacks_cards}")
+        # if len(self._stacks_cards) > 0:
+        if len(message) >0:
+            info = loads(data)
+            log_message(f"client stacks cards received: {info}")
         else:
             log_message("no client stacks cards received")
 
     
-    def server_quit(self) -> bool:
+    def server_quit(self) -> None:
         log_message("server_quit: client quitting")
-        self._send(b"quit")
-        ack:bool | None = self._receive_ack("quit accepted")
-        log_message(f"server_quit: client quit ack received: {ack}")
+        message = "quit"
+        self._send(message)
+       
         self.disconnect()
-        return self._connected
 
 if __name__ in "__main__":
     i = 0
     cc = ClientConnection()
     cc.connect()
-    for i in range(1):
-        print('.', end='', flush=True)
-        time.sleep(1)
-    cc.server_get_player_id()
+    # for i in range(1):
+    #     print('.', end='', flush=True)
+    #     time.sleep(1)
+    cc.request_player_id_from_server()
     cc.server_get_stacks_cards()
-    cc.server_get_stacks_cards()
-    cc.server_get_stacks_cards()
+    # cc.server_get_stacks_cards()
+    # cc.server_get_stacks_cards()
     cc.server_quit()
     # cc.disconnect()
     # while cc._connected:
