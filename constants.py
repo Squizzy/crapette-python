ba2c7572fe386df8c0703b2aed5669f7a4146605
Squizzy <crazy_squirrel@yahoo.com>
2025-06-08 18:33:26 +0100

    bug fix in the Players enum

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 # The enum representing the players
 class Players(Enum):
-    PLAYER1 = 0,
-    PLAYER2 = 1,
+    PLAYER1 = 0
+    PLAYER2 = 1
     CARDSTACK = 2 # mostly for debugging purposes for the moment
