2b8a8f2f8d6e93d801d6b840fdcab02a8aaadc0c
Squizzy <crazy_squirrel@yahoo.com>
2025-06-08 20:31:22 +0100

    hiding unused imports

--- pytests/tableaustack_test.py
+++ pytests/tableaustack_test.py
@@ -11,8 +11,10 @@ from player import Players
 from stacks import Stacks
 
 from samples import card_8H, card_9H, card_10H, card_7D, card_7S, card_8S, card_9S, card_QS
-from samples import card_7H_transferred_1c1, card_7H_transferred_1c2, card_7H_transferred_2c1, card_7H_transferred_1t1, card_7H_transferred_2t1
-from samples import card_9H_transferred_1c1, card_10H_transferred_1c1
+# from samples import card_7H_transferred_1c1, card_7H_transferred_1c2, card_7H_transferred_2c1, 
+# from samples import card_7H_transferred_1t1, card_7H_transferred_2t1
+from samples import card_9H_transferred_1c1
+# from samples import card_10H_transferred_1c1
 from samples import card_7D_transferred_1c1
 from samples import card_7S_transferred_1c1, card_8S_transferred_1c1, card_QS_transferred_1c1
 from samples import transferredNCards_9S_8H_7S
