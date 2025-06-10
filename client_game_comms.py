381f9b5de9ecfb90cdc3f23e49583b8f3bf5d288
Squizzy <crazy_squirrel@yahoo.com>
2025-06-10 21:40:37 +0100

    Moved the extraction of player_id and stacks_cards to the message_decode module

--- client_game_comms.py
+++ client_game_comms.py
@@ -6,6 +6,7 @@ from client_network import ClientInterface
 from network_messages import ClientNetworkMessage, ServerNetworkMessage
 # from message_encoder import encode_request
 from client_msg_encoder import ClientMessageEncoder
+from client_msg_decoder import ClientMessageDecoder
 from game_logger import GameLogger
 
 client_logger: GameLogger
@@ -39,16 +40,24 @@ class ClientGameCommsInterface(ABC):
         """Retrieve the stacks cards as sent by server"""
         pass
     
+    @abstractmethod
+    def server_quit(self) -> None:
+        """Inform the server that client is quitting"""
+        pass
+
+    
 
 class ClientGameComms(ClientGameCommsInterface):
     _connection: ClientInterface
     _message_encoder: ClientMessageEncoder
+    _message_decoder: ClientMessageDecoder
     
     def __init__(self, logger: GameLogger, connection: ClientInterface) -> None:
         global client_logger
         client_logger = logger
         self._connection = connection
         self._message_encoder = ClientMessageEncoder(logger)
+        self._message_decoder = ClientMessageDecoder(logger)
 
     def request_player_id(self) -> bool:
         """Request the server to send the player ID
@@ -81,21 +90,23 @@ class ClientGameComms(ClientGameCommsInterface):
         client_logger.info("retrieving the player_id as sent by the server")
         
         received_type: ServerNetworkMessage = ServerNetworkMessage.NONE
-        received_value: Players = Players.ERROR
+        # received_value: Players = Players.ERROR
         
         while received_type != ServerNetworkMessage.SENDING_PLAYER_ID:
             message = self._connection.receive_message()
             received_type = ServerNetworkMessage(loads(message)["type"])
                     
-        received_value = loads(message)["value"]
+        # received_value = loads(message)["value"]
 
-        try:
-            player = Players[str(received_value)]
-        except KeyError as e:
-            client_logger.error(f"received player_id is not a valid player id: {e}")
-            return Players.ERROR
+        # try:
+        #     player = Players[str(received_value)]
+        # except KeyError as e:
+        #     client_logger.error(f"received player_id is not a valid player id: {e}")
+        #     return Players.ERROR
         
-        client_logger.debug(f"{player=}")
+        # client_logger.debug(f"{player=}")
+        
+        player = self._message_decoder.decode_player_id(message)
         
         return player
 
@@ -125,15 +136,9 @@ class ClientGameComms(ClientGameCommsInterface):
     def retrieve_stacks_cards(self) -> dict[str, list[str]]:
         
         client_logger.info("retrieving the stacks cards as sent by the server")
-            
-        # received_message: dict[str, ServerNetworkMessage | str | dict]  = {
-        #     "type":"",
-        #     "target":"",
-        #     "value":{}
-        #     }
         
         received_type: ServerNetworkMessage = ServerNetworkMessage.NONE
-        received_value: dict[str, list[str]] = {}
+        # received_value: dict[str, list[str]] = {}
         
         # Wait until the proper message is received
         # TODO: This needs to be better : timeout, resend request etc...
@@ -143,17 +148,19 @@ class ClientGameComms(ClientGameCommsInterface):
             
             # client_logger.debug(f"{received_type=}")
         
-        # Extract the stacks cards from the received message
-        received_value = loads(message)["value"]
+        # # Extract the stacks cards from the received message
+        # received_value = loads(message)["value"]
 
-        # check that data was received, if not it is a problem
-        if len(received_value) > 0:
-            stacks_cards = received_value
-        else:
-            client_logger.error("received stacks cards is empty")
-            return {}
+        # # check that data was received, if not it is a problem
+        # if len(received_value) > 0:
+        #     stacks_cards = received_value
+        # else:
+        #     client_logger.error("received stacks cards is empty")
+        #     return {}
         
-        client_logger.debug(f"retrieved stacks cards: {stacks_cards}")
+        # client_logger.debug(f"retrieved stacks cards: {stacks_cards}")
+        
+        stacks_cards = self._message_decoder.decode_stacks_cards(message)
         
         return stacks_cards
         
