9f1171e67878ce927955aaaaecc4fffe1c4964fb
Squizzy <crazy_squirrel@yahoo.com>
2025-06-10 21:17:44 +0100

    updated player1/2 to 0/1 for stndardisation

--- game.py
+++ game.py
@@ -3,16 +3,16 @@ from constants import Players
 from gamestates import GameStates
 
 class Game:
+    _player0: Player
     _player1: Player
-    _player2: Player
     _player_turn: Players
     
     
     def __init__(self):
+        self._player0 = Player(Players.PLAYER0)
         self._player1 = Player(Players.PLAYER1)
-        self._player2 = Player(Players.PLAYER2)
         # TODO: randomise
-        self._player_turn = Players.PLAYER1  # Player 1 starts the game
+        self._player_turn = Players.PLAYER0  # Player 1 starts the game
         
         
     @property
@@ -20,7 +20,7 @@ class Game:
         return self._player_turn
         
     def change_turn(self) -> None:
-        self._player_turn = Players.PLAYER1 if self._player_turn == Players.PLAYER2 else Players.PLAYER2
+        self._player_turn = Players.PLAYER0 if self._player_turn == Players.PLAYER1 else Players.PLAYER1
         
     def loop(self):
         while True:
@@ -28,7 +28,7 @@ class Game:
                 break
             
             if GameStates.Player.PLAYER_BINS_REMAINDER_CARD:
-                if self.player_turn == Players.PLAYER1:
+                if self.player_turn == Players.PLAYER0:
                     # self._player1._bin.add_card(Players.PLAYER1, self._player1._remainder)
                     ...
                 
